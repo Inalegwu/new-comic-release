057d38485a1fce6edd2ed7ebfee4976f99a78400
Inalegwu Ikwue <ikwueinalegwu@gmail.com>
2024-08-14 21:12:15 +0000

    updating db schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,35 @@
-import { integer, sqliteTable, uniqueIndex } from "drizzle-orm/sqlite-core";
+import {
+  integer,
+  sqliteTable,
+  text,
+  uniqueIndex,
+} from "drizzle-orm/sqlite-core";
 
 export const release = sqliteTable(
   "release",
   {
+    id: text("id").unique().notNull(),
     releaseDate: integer("release-date", {
       mode: "timestamp",
     }).unique(),
+    url: text("url"),
   },
   (table) => ({
     releaseDateIndex: uniqueIndex("release-date-index").on(table.releaseDate),
+  }),
+);
+
+export const comingSoon = sqliteTable(
+  "coming-soon",
+  {
+    id: text("id").unique().notNull(),
+    releaseDate: integer("release-date", {
+      mode: "timestamp",
+    }),
+    url: text("url"),
+  },
+  (t) => ({
+    releaseDateIndex: uniqueIndex("release-date-index").on(t.releaseDate),
+    idIndex: uniqueIndex("id-idx").on(t.id),
   }),
 );
